c7379d992916942d8fab126652374e91c7388ba7
Md Hasibur Rahman <hasiburrahman875@gmail.com>
2023-09-30 04:53:55 -0500

    Update SimpleWordCount.scala

--- SimpleWordCount.scala
+++ SimpleWordCount.scala
@@ -1,9 +1,11 @@
-val loadfile = sc.textFile("InputFolder/input1.txt")
+val loadfile = sc.textFile("hdfs://localhost:9000/user/mrpk9/InputFolder/input1.txt")
 val words = loadfile.flatMap(line => line.split(" "))
 val wordMap = words.map(word => (word,1))
 val wordCount = wordMap.reduceByKey(_+_)
 val sorted = wordCount.sortBy(-_._2)
 val filter = sorted.filter(_._2 > 1)
-filter.saveAsTextFile("OutputFolder")
+filter.saveAsTextFile("hdfs://localhost:9000/user/mrpk9/OutputFold/")
 
-//spark-shell -i '/home/cloudera/Downloads/Spark_Demo-master/SimpleWordCount.scala'
+//spark-shell -i '/home/mrpk9/test_scala.scala'
+
+//please replace the paths according to your file locations
