5805215cac3908a3aa0fff5dae2be77c0c83248f
Md Hasibur Rahman <hasiburrahman875@gmail.com>
2025-09-21 20:08:48 -0500

    Update SimpleWordCount.scala

--- SimpleWordCount.scala
+++ SimpleWordCount.scala
@@ -1,10 +1,10 @@
-val loadfile = sc.textFile("hdfs://localhost:9000/user/mrpk9/InputFolder/input1.txt")
+val loadfile = sc.textFile("hdfs://localhost:9000/user/mrpk9/InputFolderSpark/input1.txt")
 val words = loadfile.flatMap(line => line.split(" "))
 val wordMap = words.map(word => (word,1))
 val wordCount = wordMap.reduceByKey(_+_)
 val sorted = wordCount.sortBy(-_._2)
 val filter = sorted.filter(_._2 > 1)
-filter.saveAsTextFile("hdfs://localhost:9000/user/mrpk9/OutputFold/")
+filter.saveAsTextFile("hdfs://localhost:9000/user/mrpk9/OutputFolderSparkT/")
 
 
 //Run the scala file using the following command 
